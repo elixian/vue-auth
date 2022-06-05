9f617f6d67a7fc7c3c27b2b42c1a4784ee2454b1
franck <alphacentorus@gmail.com>
2022-06-05 10:41:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <div class="container">
+    <Navbar />
+    <button @click="incrementClick">increment</button>
+    number : {{useStore.doubleCount}}
+    <router-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import { useCounterStore } from './stores/counter';
+
+const useStore = useCounterStore()
+
+function incrementClick(){
+  useStore.increment()
+}
+</script>
---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvDIoLk8YTInVbeKDS1GCX_xgYxEGywyU",
+    authDomain: "vue-auth-c4ebc.firebaseapp.com",
+    projectId: "vue-auth-c4ebc",
+    storageBucket: "vue-auth-c4ebc.appspot.com",
+    messagingSenderId: "48392066460",
+    appId: "1:48392066460:web:2004992cd382136c212459"
+  };
+
+  initializeApp(firebaseConfig);
+
+  const auth = getAuth();
